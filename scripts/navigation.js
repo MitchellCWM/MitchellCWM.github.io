053c2f582e71188377fef82c43700ac75b60c32e
Mitchell MacDonald <mitchellmacdonald@Sedins-MacBook-Pro-2.local>
2022-05-09 14:46:39 -0300

    worked on resume

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,10 +1,14 @@
 let resumeLink = document.getElementById("resumeLink");
 let randomLink = document.getElementById('randomLink');
 window.onload = () =>{
+  
     switch(document.title){
         case('Mitchell\'s Resume'): 
-          resumeLink.className = "disabledLink"
-        ; break;
+          resumeLink.className = "disabledLink";
+         break;
+        case('My test page'):
+          randomLink.className= "disabledLink";
+          break;
     }
 
 }
